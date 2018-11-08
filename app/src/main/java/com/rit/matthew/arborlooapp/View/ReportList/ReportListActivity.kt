130abedfdcc92dbb4120771e6424f0638efecb0b
Matthew Meehan <lotus.flower.matt@gmail.com>
2018-11-08 14:55:26 -0500

    Temporary fix for testing script adding duplicate data

--- app/src/main/java/com/rit/matthew/arborlooapp/View/ReportList/ReportListActivity.kt
+++ app/src/main/java/com/rit/matthew/arborlooapp/View/ReportList/ReportListActivity.kt
@@ -1,6 +1,8 @@
 package com.rit.matthew.arborlooapp.View.ReportList
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -35,77 +37,19 @@ class ReportListActivity : AppCompatActivity(), ReportListContract.View {
     private val tempData: ArrayList<ReportData> = ArrayList()
     private val moistData: ArrayList<ReportData> = ArrayList()
 
+    private lateinit var sharedPref: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.report_list_activity)
 
         presenter = ReportListPresenter(this, ReportRepository(appDB = AppDB.getInstance(this)))
 
-        val repo = ReportRepository(appDB = AppDB.getInstance(this))
-        val reportDB = ReportDB()
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE)
 
-        reportDB.name = "Arborloo 1"
-        reportDB.info = "Test Information"
-
-        repo.insertReport(reportDB, object : BaseCallback{
-            override fun onSuccess(data: MutableList<*>?) {
-                Log.d("MMMM", "Inserted")
-            }
-        })
-
-        val reportDB1 = ReportDB()
-
-        reportDB1.name = "Arborloo 2"
-        reportDB1.info = "Test Information 2"
-
-        repo.insertReport(reportDB1, object : BaseCallback{
-            override fun onSuccess(data: MutableList<*>?) {
-                Log.d("MMMM", "Inserted")
-            }
-        })
-
-        val rangeMin = 0.0
-        val rangeMax = 100.0
-        val random = Random()
-
-        var newTempData = 0.0
-        var newMoistData = 0.0
-        var newDateTime: OffsetDateTime? = null
-        var newReportId:Long = 1
-
-        val points = 200
-
-        for(i in 0..points){
-            newTempData = rangeMin + (rangeMax - rangeMin) * random.nextDouble()
-            newMoistData = rangeMin + (rangeMax - rangeMin) * random.nextDouble()
-            newDateTime = OffsetDateTime.ofInstant(Instant.ofEpochMilli(1541404810000 + (i * 3600000)), ZoneId.systemDefault())
-
-            if(i > (points/2)){
-                newReportId = 2
-            }
-
-            var newTemp = TemperatureDB()
-            var newMoist = MoistureDB()
-
-            newTemp.data = newTempData
-            newTemp.dateTime = newDateTime
-            newTemp.reportId = newReportId
-
-            newMoist.data = newMoistData
-            newMoist.dateTime = newDateTime
-            newMoist.reportId = newReportId
-
-            repo.insertTemp(newTemp, object : BaseCallback{
-                override fun onSuccess(data: MutableList<*>?) {
-
-                }
-            })
-
-            repo.insertMoist(newMoist, object : BaseCallback{
-                override fun onSuccess(data: MutableList<*>?) {
-
-                }
-            })
+        if(!checkDataExists()){
+            writeTestData()
+            writeDataExists()
         }
 
         setupUI()
@@ -173,6 +117,86 @@ class ReportListActivity : AppCompatActivity(), ReportListContract.View {
         intent.putExtra("temp", tempData)
         intent.putExtra("moist", moistData)
         startActivity(intent)
+    }
+
+    fun writeDataExists(){
+        with (sharedPref.edit()) {
+            putBoolean("hasData", true)
+            apply()
+        }
+    }
+
+    fun checkDataExists() : Boolean{
+        return sharedPref.getBoolean("hasData", false)
+    }
+
+    fun writeTestData(){
+        val repo = ReportRepository(appDB = AppDB.getInstance(this))
+        val reportDB = ReportDB()
+
+        reportDB.name = "Arborloo 1"
+        reportDB.info = "Test Information"
+
+        repo.insertReport(reportDB, object : BaseCallback{
+            override fun onSuccess(data: MutableList<*>?) {
+                Log.d("MMMM", "Inserted")
+            }
+        })
+
+        val reportDB1 = ReportDB()
+
+        reportDB1.name = "Arborloo 2"
+        reportDB1.info = "Test Information 2"
+
+        repo.insertReport(reportDB1, object : BaseCallback{
+            override fun onSuccess(data: MutableList<*>?) {
+                Log.d("MMMM", "Inserted")
+            }
+        })
+
+        val rangeMin = 0.0
+        val rangeMax = 100.0
+        val random = Random()
+
+        var newTempData = 0.0
+        var newMoistData = 0.0
+        var newDateTime: OffsetDateTime? = null
+        var newReportId:Long = 1
+
+        val points = 200
+
+        for(i in 0..points){
+            newTempData = rangeMin + (rangeMax - rangeMin) * random.nextDouble()
+            newMoistData = rangeMin + (rangeMax - rangeMin) * random.nextDouble()
+            newDateTime = OffsetDateTime.ofInstant(Instant.ofEpochMilli(1541404810000 + (i * 3600000)), ZoneId.systemDefault())
+
+            if(i > (points/2)){
+                newReportId = 2
+            }
+
+            var newTemp = TemperatureDB()
+            var newMoist = MoistureDB()
+
+            newTemp.data = newTempData
+            newTemp.dateTime = newDateTime
+            newTemp.reportId = newReportId
+
+            newMoist.data = newMoistData
+            newMoist.dateTime = newDateTime
+            newMoist.reportId = newReportId
+
+            repo.insertTemp(newTemp, object : BaseCallback{
+                override fun onSuccess(data: MutableList<*>?) {
+
+                }
+            })
+
+            repo.insertMoist(newMoist, object : BaseCallback{
+                override fun onSuccess(data: MutableList<*>?) {
+
+                }
+            })
+        }
     }
 
     override fun onDestroy() {
